1781c743557b736d850e7e5cbdbb6abd4979b45f
Santoshi-8 <santoshigouda7367@gmail.com>
2026-06-09 17:24:01 +0530

    Solved Two Sum problem

---
+++ src/Arrays/lcf.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+class lcf {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Arrays/twoSum.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class twoSum {
+    public static void main ( String[] args){
+
+       int [] nums = {2,7,11,15};
+       int target =15;
+       boolean found = false;
+
+
+         for (int i=0; i< nums.length; i++){
+            for (int j= i+1; j< nums.length; j++){
+                if (nums[i]+ nums[j]==target){
+                    System.out.println("output at [" + i+","+j +"]");
+                    found = true;
+                    break;
+                } // if
+            }// inner for loop
+         }// outer for loop
+        if(!found){
+            System.out.println("target not found");
+            
+        }
+
+
+    }
+
+}
+
+
+
